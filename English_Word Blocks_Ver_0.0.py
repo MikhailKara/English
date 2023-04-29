a0a99fa56eafd1b7bbfcce82bee03f180d1ae0e5
MikhailKara <126684130+MikhailKara@users.noreply.github.com>
2023-04-29 18:19:13 +0300

    Add files via upload
    Initial version based on if without while loop!

---
+++ English_Word Blocks_Ver_0.0.py
@@ -0,0 +1,65 @@
+table13 = ['wet', 'caught','fragile','be not used to', 'come and hold my hand']
+table13ru = ["мокрый", "пойманый", "Хрупкий", "не привыкнуть", "Приди и возьми мою руку"]
+
+print('Отрабатываем задания по таблице 13')
+#Пошли блоки по конкретным заданиям
+a = 1
+while a== 1:
+    print(table13ru[0], ("как ?"))
+    a = input("Введи слово: ")
+    if a == table13[0]:
+        print("Верно!")
+    if a != table13[0]:
+        a = 1
+        print("Не верно!")
+#Конец блока первого слова
+a = 1
+while a== 1:
+    print(table13ru[1], ("как ?"))
+    a = input("Введи слово: ")
+    if a == table13[1]:
+        print("Верно!")
+    if a != table13[1]:
+        a = 1
+        print("Не верно!")
+a = 1
+while a== 1:
+    print(table13ru[2], ("как ?"))
+    a = input("Введи слово: ")
+    if a == table13[2]:
+        print("Верно!")
+    if a != table13[2]:
+        a = 1
+        print("Не верно!")
+a = 1
+while a== 1:
+    print(table13ru[3], ("как ?"))
+    a = input("Введи слово: ")
+    if a == table13[3]:
+        print("Верно!")
+    if a != table13[3]:
+        a = 1
+        print("Не верно!")
+a = 1
+while a== 1:
+    print(table13ru[4], ("как ?"))
+    a = input("Введи слово: ")
+    if a == table13[4]:
+        print("Верно!")
+    if a != table13[4]:
+        a = 1
+        print("Не верно!")
+b = 'Задания выполнины'
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
