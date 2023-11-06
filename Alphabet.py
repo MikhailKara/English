818e5cc8f536b1cd49e7b071c6135bb56863b6b2
MikhailKara <126684130+MikhailKara@users.noreply.github.com>
2023-11-06 17:30:08 +0300

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,203 @@
+info_patch = ["patch V-1.0 from November 06 , 2023"]
+info_last_patch = ["записываютчя предпоследние патчи"]
+Aa = ['abandon',	'achieve',	'actually',	'aid',	'allow',	'already',	'alternate',	'anniversary',	'anvil',	'application',	'apply for',	'array',	'arrow',	'assault',	'attachment',	'attempt',	'avoid',	'avoiding',]
+A_ru = ['бросать, отказываться',	'достигать',	'фактически',	'помощь ',	'позволять',	'уже',	'попеременный',	'годовщина',	'наковальня',	'приложение',	'подавать заявление на принятие на работу',	'массив',	'стрелка',	'атака',	'приспособление',	'попытка',	'избегать',	'избегающий',]
+Bb = ['backpack',	'bang',	'bay',	'be not used to',	'blame',	'bless',	'bloom',	'boost',	'bought',	'bounty',	'bring up', 'build',]
+B_ru = ['рюкзак',	'взрыв',	'бухта',	'не привыкнуть',	'вина',	'благословить',	'цвести',	'повышение, рост',	'купленный',	'награда',	'вызвать', 'сборка',]
+Cc = ['carry',	'case',	'cave',	'chairman',	'chosen',	'closer',	'come',	'come and hold my hand',	'commodities',	'compartments',	'competitor',	'complicated',	'composition',	'complicated',	'contagion',	'contributions',	'court order',	'crave',	'crossbow',	'customer support',	'cutter',]
+C_ru = ['нести',	'случай',	'пещера',	'председатель',	'выбранный',	'ближе',	'приходить',	'подойди и возьми меня за руку',	'товары',	'отсеки',	'конкурент',	'сложный',	'композиция',	'сложный',	'инфекция',	'взнос',	'судебный приказ',	'жаждать',	'арбалет',	'служба поддержки клиентов',	'резак',]
+Dd = ['dawn',	'demand',	'den',	'density',	'detected',	'determine',	'determines',	'disappearance',	'dismiss',	'dismissal',	'dissipate',	'doubt',	'duration',]
+D_ru = ['рассвет',	'требование',	'логово',	'плотность',	'обнаруженный',	'определять',	'определяет',	'исчезновение',	'увольнять',	'увольнение',	'рассеиваться',	'сомнение',	'продолжительность',]
+Ee = ['each other',	'embarrass',	'engine',	'entirely',	'equipped',	'estimate',	'ethnicity',	'expect',	'external',	'extraction',]
+Ee_ru = ['друг друга',	'смущать',	'двигатель',	'полностью',	'оборудованный',	'подсчитывать приблизительно',	'этническая принадлежность',	'ожидать',	'внешний',	'добыча,извлечение',]
+Ff = ['facility',	'fall ill',	'fill in for',	'fingertip',	'firmware',	'flat',	'flatting',	'fluency',	'fly',	'following',	'for',	'fragile',	'framework',	'fun',]
+Ff_ru = ['объект',	'заболеть',	'подменять (кого-то на работе)',	'кончик пальца',	'прошивку',	'плоско',	'выравнивание',	'свободное владение',	'летать',	'после, следующий',	'для',	'хрупкий',	'рамки',	'веселье',]
+Gg = ['gear',	'get back',	'get started',	'get wet',	'give birth to',	'glutes',]
+Gg_ru = ['передача',	'возвращаться',	'начинать',	'мокнуть',	'производить на свет',	'ягодичная мышца',]
+Hh = ['hamstring',	'haymaker',	'hire',	'horse',	'hurricane',]
+Hh_ru = ['подколенное сухожилие',	'косарь',	'нанимать',	'лошадь',	'ураган',]
+Ii = ['identify',	'in-demand',	'incapacitated',	'injury',	'injustice',	'inner',]
+Ii_ru = ['идентифицировать',	'востребованный',	'выведенный из строя',	'травма',	'несправедливость',	'внутренний',]
+Jj = ['jab',	'just',]
+Jj_ru = ['удар',	'просто',]
+Ll = ['lately',	'list',	'live',	'look forward to',	'lucrative',	'leaked',]
+Ll_ru = ['недавно (несколько раз)',	'список',	'жить',	'ждать с нетерепением',	'прибыльный',	'утечка',]
+Mm = ['maintenance',	'means',	'mercenary',	'mineral deposit',	'modifier',]
+Mm_ru = ['обслуживание',	'средства',	'наемник',	'месторождение полезных ископаемых',	'модификатор',]
+Nn = ['newimage',	'noimage',	'nomad',	'not entirely',	'noticeably',]
+Nn_ru = ['новое изображение',	'нет изображения',	'кочевник',	'не совсем',	'заметно',]
+Oo = ['obstructing',	'offender',	'on demand',	'out of the blue',	'outbreak',	'overall',]
+Oo_ru = ['препятствующий',	'правонарушитель',	'по требованию',	'внезапно',	'вспышка',	'общий',]
+Pp = ['part-time',	'particularly',	'peers',	'performing',	'perseverance',	'pressure',	'processing',	'prolate',	'prone',	'properly',	'pros',	'proven',]
+Pp_ru = ['частичная занятость',	'особенно',	'сверстникам',	'исполняемый',	'настойчивость',	'давление',	'обработка',	'вытянутый',	'лежащий ничком',	'правильно',	'профи',	'доказанный',]
+Qq = ['queue',	'quit',]
+Qq_ru = ['очередь',	'покидать',]
+Rr = ['rated',	'refuse',	'rental',	'report',	'request',	'required',	'residence',	'rectricted',]
+Rr_ru = ['номинальный',	'отказываться',	'сдаваемый внаем',	'доклад',	'Запрос',	'необходимый',	'местожительство',	'закрытый',]
+Ss = ['scope',	'sentinel',	'sequence',	'sharpshooter',	'sidearm',	'solace',	'specimen',	'statement',	'strut',	'susceptible',]
+Ss_ru = ['диапазон',	'часовой',	'последовательность',	'снайпер',	'пистолет',	'утешение',	'образец',	'заявление',	'стойка , подпора',	'восприимчивый',]
+Tt = ['take a picture',	'therefore',	'throw',	'torso',]
+Tt_ru = ['фотографировать',	'поэтому',	'бросать',	'туловище',]
+Uu = ['unspecified',	'untrack',]
+Uu_ru = ['неуточненный',	'отменить отслеживание',]
+Vv = ['vantage',	'vigilante',	'vitals',]
+Vv_ru = ['выгодная позиция',	'линчеватель',	'жизненно важныеорганы',]
+Ww = ['wave',	'wax',	'way to go',	'wet',	'while',	'willing',	'withdraw',	'withdraw money',]
+Ww_ru = ['волна',	'воск',	'так держать!',	'мокрый',	'в то время как',	'готовый',	'изымать',	'выводить деньги',]
+Yy = ['yet',]
+Yy_ru = ['ещё',]
+#ошибка была по тому что A and B уже были заняты и являлись переменными по этому G не была ровна A
+End = []
+End_ru = []
+Aaa = "A"
+Bbb = "B"
+Ccc = "C"
+Ddd = "D"
+Eee = "E"
+Fff = "F"
+Ggg = "G"
+Hhh = "H"
+Iii = "I"
+Jjj = "J"
+Lll = "L"
+Mmm = "M"
+Nnn = "N"
+Ooo = "O"
+Ppp = "P"
+Qqq = "Q"
+Rrr = "R"
+Sss = "S"
+Ttt = "T"
+Uuu = "U"
+Vvv = "V"
+Www = "W"
+Yyy = "Y"
+op = 0
+op_ru = 0
+print("Хочеш узнать версию патча?")
+p = input("Если да, то нажмите //'y'// или //д//! Если нет нажмите //Enter//")
+if p == "y":
+    print(info_patch)
+if p == "д":
+    print(info_patch)
+j = 0
+while j == 0:
+    g = input("Выбери букву:")
+    if g != Aaa or Bbb or Ccc or Ddd or Eee or Fff or Ggg or Hhh or Iii or Jjj or Lll or Mmm or Nnn or Ooo or Ppp or Qqq or Rrr or Sss or Ttt or Uuu or Vvv or Www or Yyy:
+        op = End
+        op_ru = End_ru
+    if g == Aaa:
+        op = Aa
+        op_ru = A_ru
+    if g == Bbb:
+        op = Bb
+        op_ru = B_ru
+    if g == Ccc:
+        op = Cc
+        op_ru = C_ru
+    if g == Ddd:
+        op = Dd
+        op_ru = D_ru
+    if g == Eee:
+        op = Ee
+        op_ru = Ee_ru
+    if g == Fff:
+        op = Ff
+        op_ru = Ff_ru
+    if g == Ggg:
+        op = Gg
+        op_ru = Gg_ru
+    if g == Hhh:
+        op = Hh
+        op_ru = Hh_ru
+    if g == Iii:
+        op = Ii
+        op_ru = Ii_ru
+    if g == Jjj:
+        op = Jj
+        op_ru = Jj_ru
+    if g == Lll:
+        op = Ll
+        op_ru = Ll_ru
+    if g == Mmm:
+        op = Mm
+        op_ru = Mm_ru
+    if g == Nnn:
+        op = Nn
+        op_ru = Nn_ru
+    if g == Ooo:
+        op = Oo
+        op_ru = Oo_ru
+    if g == Ppp:
+        op = Pp
+        op_ru = Pp_ru
+    if g == Qqq:
+        op = Qq
+        op_ru = Qq_ru
+    if g == Rrr:
+        op = Rr
+        op_ru = Rr_ru
+    if g == Sss:
+        op = Ss
+        op_ru = Ss_ru
+    if g == Ttt:
+        op = Tt
+        op_ru = Tt_ru
+    if g == Uuu:
+        op = Uu
+        op_ru = Uu_ru
+    if g == Vvv:
+        op = Vv
+        op_ru = Vv_ru
+    if g == Www:
+        op = Ww
+        op_ru = Ww_ru
+    if g == Yyy:
+        op = Yy
+        op_ru = Yy_ru
+    i = 0
+    if len(op) != len(op_ru):
+        print("Где то забыл слово!")
+        i = 0
+        size = 0
+    if len(op) == len(op_ru):
+        size = len(op)
+        print('Отрабатываем задания по Алфавиту')
+    while i < size:
+        a = 1
+        while a == 1:
+            print(op_ru[i], ":как? Проговори вслух!")
+            a = input("Введи слово: ")
+            if a == op[i]:
+                print("Верно!")
+            if a != op[i]:
+                a = 1
+                print("Не верно!")
+                b = input("Нужна подсказка? Если да, то нажмите //'y'// или //д//! Если нет нажмите //Enter//")
+                if b == "y":
+                    print(op[i])
+                if b == "д":
+                    print(op[i])
+        i += 1
+        if i == size:
+            c = input("Хотите повторить? Если да, то нажмите //'y'// или //д//! Если нет нажмите //Enter//")
+            if c == "y":
+                i = 0
+                print("Итак! Повторим!")
+            if c == "д":
+                i = 0
+                print("Итак! Повторим!")
+            b = 'Задания выполнены'
+            print(b)
+    d = input("Хочешь новую букву? Если да, то нажмите //'y'// или //д//! Если нет нажмите //Enter//")
+    #Разбери ошибку почему не получалось окончить цикл и почему для окончания цикла в начале надо ставить лож!
+    j = 1
+    if d == "y":
+        j = 0
+        print("Итак! Выберим новую букву!")
+    if d == "д":
+        j = 0
+        print("Итак! Выберим новую букву!")
+print("До новых встреч!")
+
+
